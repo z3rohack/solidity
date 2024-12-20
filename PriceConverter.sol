90cd28b57b80a5d1f2ce9b86bc3b18ef8c2e1e3c
z3rohack <hi@iotaprice.com>
2024-12-20 12:58:05 +0900

    Price Converter

---
+++ PriceConverter.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+
+      function getPrice() internal view returns(uint256){
+        // this function returns value of ETH in USD
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306 ETH price feed contract from chainlink sepolia price feed contracts
+        // ABI 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD 2000.00000000
+        return uint256(answer * 1e10); //returns as uint256 from int256
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }    
+
+        function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+        
+        }
+}
